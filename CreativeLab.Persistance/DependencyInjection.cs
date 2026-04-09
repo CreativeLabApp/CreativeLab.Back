463db88da9ae092166a1d5833dd10e3eb820490d
artxmix <artem.artusevskij01@gmail.com>
2026-04-10 00:01:39 +0300

    save

---
+++ CreativeLab.Persistance/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using CreativeLab.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CreativeLab.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        var serverVersion = new MySqlServerVersion(new Version(configuration["DatabaseSettings:ServerVersion"]
+            ?? throw new NullReferenceException("server version was null")));
+
+        services.AddDbContext<CreativeLabDbContext>(options =>
+        {
+            options.UseMySql(
+                connectionString,
+                serverVersion,
+                mySqlOptions => mySqlOptions.EnableStringComparisonTranslations()
+            );
+        });
+
+        services.AddScoped<ICreativeLabDbContext>(provider =>
+            provider.GetService<CreativeLabDbContext>()
+                ?? throw new NullReferenceException("provider cant't be null"));
+
+        return services;
+    }
+}
